d5e9658415e28e96beda7b90750b135bde93d99a
MNR_Dev <mnrdev510@gmail.com>
2022-04-13 21:48:44 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import Region from "./Components/region";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [data, setData] = useState([]);
+
+  const options = {
+    method: "GET",
+    url: "https://covid-19-statistics.p.rapidapi.com/reports/total",
+    params: { date: "2020-04-07" },
+    headers: {
+      "X-RapidAPI-Host": "covid-19-statistics.p.rapidapi.com",
+      "X-RapidAPI-Key": "53fd323a60msh0a9927ee29aa502p11763ajsnd24449e9c20b",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      return(setData(response.data))
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+  return <div className='app'>
+    {data.confirmed.map((region, index) => {
+      console.log(region)
+    })}
+  </div>;
 }
 
 export default App;
---
+++ src/Components/region.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Region(props) {
+    console.log(props.data);
+  return (
+    <div>Region</div>
+  )
+}
+
+export default Region;
