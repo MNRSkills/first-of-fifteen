c418ffb2b68a0db217902749a52d254f3fbeb0e1
MNR_Dev <mnrdev510@gmail.com>
2022-04-15 17:15:00 -0700

    connected personal api, now using dummy data

--- src/App.js
+++ src/App.js
@@ -5,10 +5,71 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState({});
+  const stats = {
+    response :[
+      {
+        contenant: "North-America",
+        country: "Saint-Pierre-Miquelon",
+        population: 5744,
+      }
+    ]
+  }
 
-  const axios = require("axios");
-
-  
+  const funkData = {
+    post: [
+      {
+       country: "usa",
+       population: "3434345353",
+       cases: {
+         new: "+20955",
+         active: "11220499",
+         critical: "32333",
+         recovered: "2334455",
+         total: 2004498
+       },
+       deaths: {
+        new: "41122",
+        M_pop: "327",
+        total: 108055
+      },
+        iso: "122",
+      },
+      {
+       country: "usa",
+       population: "3434345353",
+       cases: {
+         new: "+20955",
+         active: "11220499",
+         critical: "32333",
+         recovered: "2334455",
+         total: 2004498
+       },
+       deaths: {
+         new: "41122",
+         M_pop: "327",
+         total: 108055
+       },
+        iso: "122",
+      },
+      {
+       country: "usa",
+       population: "3434345353",
+       cases: {
+         new: "+20955",
+         active: "11220499",
+         critical: "32333",
+         recovered: "2334455",
+         total: 2004498
+       },
+       deaths: {
+        new: "41122",
+        M_pop: "327",
+        total: 108055
+      },
+        iso: "122",
+      },
+    ],
+  };
   const callAPI = async () => {
     try {
       await axios
@@ -23,13 +84,15 @@ function App() {
     } catch (error) {}
   };
 
-  useEffect(() => {
-    callAPI();
-  }, []);
+  // useEffect(() => {
+  //   callAPI();
+  // }, []);
 
   return (
     <div className='app'>
-      <Region />
+      {funkData.post.map((title, index) => {
+        return <Region title={title} key={index}/>
+      })}
     </div>
   );
 }
--- src/Components/region.js
+++ src/Components/region.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Card, CardContent, CardActions, Button} from '@mui/material';
 
 function Region(props) {
-    // console.log(props.data);
+    console.log(props.title);
   return (
-    <div>Region</div>
+    <div>
+     <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {props.title.Name}
+        </Typography>
+       
+        
+       
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </div>
   )
 }
 
